8160fc61810845db9c0b077b0f0fd4829b6f69a0
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-07-08 16:09:28 +0300

    Modify the add method to accept any number of numbers in the string sequence

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -9,14 +9,13 @@ namespace StringCalculatorKata
             if (!sequence.Any())
                 return 0;
             var numberStrings = sequence.Split(',');
-            switch (numberStrings.Length)
+            int sum = 0;
+            foreach (var numberString in numberStrings)
             {
-                case 1:
-                    return ParseInt(numberStrings[0]);
-                case 2:
-                    return ParseInt(numberStrings[0]) + ParseInt(numberStrings[1]);
-                default: return -1;
+                sum += ParseInt(numberString);
             }
+
+            return sum;
         }
 
         private static int ParseInt(string numberString)
