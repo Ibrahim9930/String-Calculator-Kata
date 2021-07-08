e776c8455481b1c741e110ffb82f772541c9b397
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-07-08 19:30:08 +0300

    Refactor add method in string calculator

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -6,33 +6,61 @@ namespace StringCalculatorKata
 {
     public class StringCalculator
     {
+        private static readonly Regex CustomDelimiterRegex = new Regex("(?<=(//)).*");
+        private const string DefaultDelimiter = ",|\n";
+
         public int Add(string sequence)
         {
             if (!sequence.Any())
                 return 0;
-            var numberStrings = ExtractNumberStrings(ref sequence);
+            var numberStrings = ExtractNumberStrings(sequence);
             return AddNumbersInAStringSequence(numberStrings);
         }
 
-        private static string[] ExtractNumberStrings(ref string sequence)
+        private static string[] ExtractNumberStrings(string sequence)
         {
-            Regex separatorRegex = new Regex("(?<=(//)).*");
-
-            var firstLine = sequence.Split("\n").FirstOrDefault();
-            string pattern;
-            if (separatorRegex.IsMatch(firstLine))
-            {
-                var match = separatorRegex.Match(firstLine);
-                pattern = match.Value;
-                sequence = sequence.Remove(0, firstLine.Length + 1);
-            }
-            else
-            {
-                pattern = ",|\n";
-            }
-            return Regex.Split(sequence,pattern);
+            string separator = GetSeparator(sequence);
+            RemoveCustomDelimiterSpecification(ref sequence);
+            return Regex.Split(sequence, separator);
         }
 
+        private static string GetSeparator(string sequence)
+        {
+            if (HasCustomDelimiter(sequence))
+            {
+                return GetCustomDelimiter(sequence);
+            }
+
+            return DefaultDelimiter;
+        }
+
+        private static bool HasCustomDelimiter(string sequence)
+        {
+            var firstLine = GetFirstLine(sequence);
+
+            return CustomDelimiterRegex.IsMatch(firstLine);
+        }
+        
+        private static string GetCustomDelimiter(string sequence)
+        {
+            var firstLine = GetFirstLine(sequence);
+            return CustomDelimiterRegex.Match(firstLine).Value;
+        }
+        
+        private static string GetFirstLine(string sequence)
+        {
+            return sequence.Split("\n").FirstOrDefault();
+        }
+
+        private static void RemoveCustomDelimiterSpecification(ref string sequence)
+        {
+            if (HasCustomDelimiter(sequence))
+            {
+                var firstLine = GetFirstLine(sequence);
+                sequence = sequence.Remove(0, firstLine.Length + 1);
+            }
+        }
+        
         private static int AddNumbersInAStringSequence(string[] numberStrings)
         {
             int sum = 0;
